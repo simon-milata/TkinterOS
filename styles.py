0f4523b73538d566349778191c20c8c90daa43f3
Tamzuu <milata.simon@gmail.com>
2024-03-11 23:38:55 +0100

    Create styles.py

---
+++ styles.py
@@ -0,0 +1,17 @@
+python_blue = "#326c9b"
+python_yellow = "#ffe66d"
+
+light_font_color = "black"
+dark_font_color = "white"
+
+font_color = (light_font_color, dark_font_color)
+
+light_button_color = "orange"
+dark_button_color = "orange"
+
+button_color = (light_button_color, dark_button_color)
+
+light_button_font_color = "black"
+dark_button_font_color = "black"
+
+button_font_color = (light_button_font_color, dark_button_font_color)
