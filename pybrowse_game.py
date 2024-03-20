37273a3f004749728dd62781b8906c4040d62aeb
Tamzuu <milata.simon@gmail.com>
2024-03-20 19:16:40 +0100

    Feat: Add Pybrowse Game Python Jump And Game Over
    - Add function to make python jump and fall
    - Add simple mechanism to end game on barrier touch

--- pybrowse_game.py
+++ pybrowse_game.py
@@ -12,10 +12,8 @@ class PyBrowseGame:
         self.game_frame = game_frame
         
         self.create_icons()
-        self.create_variables()
-        self.create_python()
 
-        
+        self.create_python()
 
 
     def create_icons(self) -> None:
@@ -23,73 +21,156 @@ class PyBrowseGame:
 
 
     def create_python(self) -> None:
+        self.game_frame.update()
+
         self.python_spawn_pos_x = int(self.game_frame.winfo_width() * 0.1)
         self.python_spawn_pos_y = int(self.game_frame.winfo_height() * 0.7)
         self.python = ctk.CTkLabel(self.game_frame, image=self.python_icon, fg_color="transparent", text="")
         self.python.place(x=self.python_spawn_pos_x, y=self.python_spawn_pos_y)
 
+        self.python_x = self.python_spawn_pos_x
+        self.python_y = self.python_spawn_pos_y
+        self.python_width = 64
+        self.python_height = 64
+
 
     def start_game(self) -> None:
+        self.create_variables()
+
+        self.game_frame.grab_set()
+        self.game_frame.focus_set()
         self.create_binds()
-        self.game_frame.after(1000, self.spawn_object)
+        self.game_frame.after(1000, self.spawn_barrier)
 
 
     def create_variables(self):
-        self.game_over = False
-        self.spawn_frequency = 0.5
-        self.error_list = ["ValueError", "IndexError", "ZeroDivisionError", "SyntaxError", "IndentationError", "TypeError", "NameError"]
         self.game_frame.update()
+        self.game_over = False
+        self.barrier_list = []
+        self.barrier_frequency = 1
         self.spawn_pos_x = int(self.game_frame.winfo_width())
         self.spawn_pos_y = 0
         self.spawn_pos_y_low = int(self.game_frame.winfo_height() * 0.8)
-        self.barrier_speed = 5
+        self.barrier_speed = 3
+        self.mid_air = False
+        
 
 
-    def spawn_object(self) -> None:
+    def spawn_barrier(self) -> None:
         if self.game_over:
             return
         
-        up_or_down = random.choice(["up", "down"])
+        up_or_down = random.choice(["up", "down", "down", "down"])
         
         if up_or_down == "down":
-            barrier = ctk.CTkLabel(self.game_frame, height=self.game_frame.winfo_height() / 3, text=random.choice(self.error_list), fg_color=button_color, 
-                                   text_color=button_font_color, font=(font_family, font_size_small))
-            barrier.place(x=self.spawn_pos_x, y=self.spawn_pos_y_low)
+            spawn_pos_y = self.spawn_pos_y_low
         else:
-            barrier = ctk.CTkLabel(self.game_frame, text=random.choice(self.error_list), fg_color=button_color, text_color=button_font_color, 
-                                   font=(font_family, font_size_small))
-            barrier.place(x=self.spawn_pos_x, y=self.spawn_pos_y)
+            spawn_pos_y = self.spawn_pos_y
 
-        self.move_object(barrier)
+        if self.barrier_speed > 2:
+            self.barrier_speed -= 0.01
 
-        if self.game_over:
-            return
+        Blockade(self, self.game_frame, self.spawn_pos_x, spawn_pos_y, self.barrier_speed)
 
-        if self.barrier_speed > 1:
-            self.barrier_speed -= 0.1
+        if self.barrier_frequency > 0.3:
+            self.barrier_frequency -= 0.01
 
-    
-    def move_object(self, blockade: ctk.CTkLabel):
-        blockade.update()
-        x_pos = blockade.winfo_x()
-        x_pos -= 1
-        y_pos = blockade.winfo_y()
-        blockade.place(x=x_pos, y=y_pos)
-        
-        if x_pos <= blockade.winfo_width():
-            blockade.destroy()
-            self.spawn_object()
+        self.game_frame.after(int(1000 * self.barrier_frequency), self.spawn_barrier)
+
+
+    def jump(self, event) -> None:
+        if self.mid_air:
             return
         
-        blockade.after(int(self.barrier_speed), lambda: self.move_object(blockade))
+        self.mid_air = True
+
+        self.move_up()
 
 
-    def jump(self) -> None:
-        self.python.update()
-        print(self.python.winfo_y)
+    def move_up(self) -> None:
+        if self.python_y > 0:
+            self.python_y -= 1
+            self.python.place(x=self.python_x, y=self.python_y)
+        else:
+            self.python.after(100, self.move_down)
+            return
+            
+        self.python.after(1, self.move_up)
+
+
+    def move_down(self) -> None:
+        if self.python_y < self.python_spawn_pos_y:
+            self.python_y += 1
+            self.python.place(x=self.python_x, y=self.python_y)
+        else:
+            self.mid_air = False
+            return
+        
+        self.python.after(1, self.move_down)
 
 
     def create_binds(self) -> None:
-        self.game_frame.bind("<space>", self.jump)
-        self.game_frame.bind("w", self.jump)
-        self.game_frame.bind("up", self.jump)
+        self.game_frame.bind("<Button-1>", self.jump)
+
+    
+    def end_game(self) -> None:
+        self.game_over = True
+        self.game_frame.unbind("<Button-1>")
+        print("Game Over")
+        
+        for barrier in self.barrier_list:
+            barrier.destroy()
+
+        
+
+
+class Blockade:
+    def __init__(self, game: PyBrowseGame, game_frame: ctk.CTkFrame, x_pos: int, y_pos: int, speed: int) -> None:
+        self.game = game
+        self.game_frame = game_frame
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.speed = speed
+
+        self.error_list = ["ValueError", "IndexError", "ZeroDivisionError", "SyntaxError", "IndentationError", "TypeError", "NameError"]
+
+        self.create_barrier()
+        self.get_params()
+        self.move_barrier()
+
+
+    def create_barrier(self) -> ctk.CTkLabel:
+        self.barrier = ctk.CTkLabel(self.game_frame, height=self.game_frame.winfo_height() / 3, text=random.choice(self.error_list), fg_color=button_color, 
+                                   text_color=button_font_color, font=(font_family, font_size_small))
+        self.barrier.place(x=self.x_pos, y=self.y_pos)
+
+        self.game.barrier_list.append(self.barrier)
+    
+
+    def get_params(self) -> None:
+        self.barrier.update()
+        self.width = self.barrier.winfo_width()
+        self.height = self.barrier.winfo_height()
+        self.barrier_x = self.x_pos
+        self.barrier_y = self.y_pos
+
+
+    def move_barrier(self):
+        self.barrier_x -= 1
+        self.barrier.place(x=self.barrier_x, y=self.barrier_y)
+            
+        if self.barrier_x <= -self.width:
+            self.game.barrier_list.pop(0)
+            self.barrier.destroy()
+            return
+        
+        if (self.game.python_x < self.barrier_x + self.width and
+            self.game.python_x + self.game.python_width > self.barrier_x and
+            self.game.python_y < self.barrier_y + self.height and
+            self.game.python_y + self.game.python_height > self.barrier_y):
+            if not self.game.game_over:
+                self.game.end_game()
+                return
+                
+            
+        self.barrier.after(int(self.speed), self.move_barrier)
