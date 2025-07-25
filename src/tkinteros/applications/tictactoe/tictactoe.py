278c14515cdc27582827c7b9622405312b02fcae
Simon Milata <milata.simon@gmail.com>
2025-07-25 21:36:47 +0200

    Added replay functionality, full gameplay loop now functional - Added a replay functionality - Standalone game fully playable

--- src/tkinteros/applications/tictactoe/tictactoe.py
+++ src/tkinteros/applications/tictactoe/tictactoe.py
@@ -1,5 +1,3 @@
-from copy import deepcopy
-
 import customtkinter as ctk
 
 from tkinteros.applications.tictactoe.tictactoe_gui import TicTacToeGUI
@@ -12,18 +10,31 @@ class TicTacToe:
         pass
 
 
-    def run(self):
+    def setup(self):
         self.create_variables()
-        self.gui = TicTacToeGUI()
-        self.bot = TicTacToeBot(random_start=True)
-        self.create_board()
+        self.gui = TicTacToeGUI(start_callback=self.start_game, replay_callback=self.reset_game)
         self.gui.run()
+
+
+    def start_game(self, grid_size):
+        grid_size = int(grid_size.split("x")[0])
+
+        self.gui.hide_main_menu()
+        print(self.get_points_to_win(grid_size))
+        self.bot = TicTacToeBot(random_start=True, points_to_win=self.get_points_to_win(grid_size))
+        self.create_board(rows=grid_size, columns=grid_size)
+
+
+    def get_points_to_win(self, grid_size: int) -> int:
+        if grid_size == 3:
+            return 3
+        elif grid_size == 5:
+            return 4
 
 
     def create_variables(self):
         self.board_list = []
         self.button_list = []
-        self.box_size = 40
         self.base_color1 = "#ffd36b"
         self.base_color2 = "#104b98"
         self.cell_size = 40
@@ -39,11 +50,29 @@ class TicTacToe:
 
         self.print_board(self.board_list)
 
-        if self.evaluate_game_state(self.board_list, self.empty_cell):
-            # TODO: end game
-            self.gui.window.destroy()
+        grid_size = len(self.board_list)
+
+        winner = self.evaluate_game_state(self.board_list, self.empty_cell, self.get_points_to_win(grid_size))
+        if winner:
+            header = f"{winner.upper()} won!" if winner != "tie" else "Tie!"
+            self.gui.create_game_over_menu(header)
+            return
+
         self.switch_player()
         self.bot_take_turn()
+
+
+    def reset_game(self):
+        self.gui.hide_game_over_menu()
+        self.gui.show_main_menu()
+
+        for row in self.button_list:
+            for button in row:
+                button.destroy()
+
+        self.board_list = []
+        self.button_list = []
+        self.current_player = self.player_1
 
 
     def bot_take_turn(self):
@@ -54,16 +83,19 @@ class TicTacToe:
         self.gui.check_cell(cell_button=button, player=self.current_player)
         self.switch_player()
         self.print_board(self.board_list)
-        self.evaluate_game_state(board_list=self.board_list)
+
+        winner = self.evaluate_game_state(self.board_list, self.empty_cell, points_to_win=(self.get_points_to_win(len(self.board_list))))
+        if winner:
+            header = f"{winner.upper()} won!" if winner != "tie" else "Tie!"
+            self.gui.create_game_over_menu(header)
+            return
 
 
     def evaluate_winner(self, x_count: int, o_count: int, points_to_win: int = 4):
         if x_count >= points_to_win:
-            print("X WON")
             return "X"
             
         elif o_count >= points_to_win:
-            print("O WON")
             return "O"
 
         return None
@@ -83,13 +115,16 @@ class TicTacToe:
             for cell in row:
                 if cell == empty_cell:
                     return False
-        print("DRAW")
         return True
 
 
-    def evaluate_game_state(self, board_list: list, empty_cell: str = " ", board_size: int = 5):
+    def evaluate_game_state(self, board_list: list, empty_cell: str = " ", points_to_win: int = 4):
+        board_size = len(board_list)
+
         if self.is_draw(board_list, empty_cell):
-            return "draw"
+            return "tie"
+        
+        points_to_win = self.get_points_to_win(board_size)
         
         # horizontal
         for i in range(board_size):
@@ -99,9 +134,9 @@ class TicTacToe:
                 current_cell = board_list[i][j]
                 x_count, o_count = self.evaluate_cell(current_cell, x_count, o_count)
 
-                if self.evaluate_winner(x_count, o_count):
-                    print("horizontal")
-                    return self.evaluate_winner(x_count, o_count)
+                winner = self.evaluate_winner(x_count, o_count, points_to_win)
+                if winner:
+                    return winner
 
         # vertical
         for i in range(board_size):
@@ -111,9 +146,9 @@ class TicTacToe:
                 current_cell = board_list[j][i]
                 x_count, o_count = self.evaluate_cell(current_cell, x_count, o_count)
 
-                if self.evaluate_winner(x_count, o_count):
-                    print("vertical")
-                    return self.evaluate_winner(x_count, o_count)
+                winner = self.evaluate_winner(x_count, o_count, points_to_win)
+                if winner:
+                    return winner
 
         # diagonal \\ up
         for i in range(board_size):
@@ -123,9 +158,9 @@ class TicTacToe:
                 current_cell = board_list[j+i][j]
                 x_count, o_count = self.evaluate_cell(current_cell, x_count, o_count)
 
-                if self.evaluate_winner(x_count, o_count):
-                    print("diagonal \\ up")
-                    return self.evaluate_winner(x_count, o_count)
+                winner = self.evaluate_winner(x_count, o_count, points_to_win)
+                if winner:
+                    return winner
 
         # diagonal \ down
         for i in range(board_size):
@@ -135,9 +170,9 @@ class TicTacToe:
                 current_cell = board_list[j][j+i]
                 x_count, o_count = self.evaluate_cell(current_cell, x_count, o_count)
 
-                if self.evaluate_winner(x_count, o_count):
-                    print("diagonal \\ down")
-                    return self.evaluate_winner(x_count, o_count)
+                winner = self.evaluate_winner(x_count, o_count, points_to_win)
+                if winner:
+                    return winner
                 
         # diagonal / left
         for i in range(board_size):
@@ -147,9 +182,9 @@ class TicTacToe:
                 current_cell = board_list[board_size-j-i-1][j-i]
                 x_count, o_count = self.evaluate_cell(current_cell, x_count, o_count)
 
-                if self.evaluate_winner(x_count, o_count):
-                    print("diagonal / up")
-                    return self.evaluate_winner(x_count, o_count)
+                winner = self.evaluate_winner(x_count, o_count, points_to_win)
+                if winner:
+                    return winner
 
         # diagonal / right
         for i in range(board_size):
@@ -159,9 +194,9 @@ class TicTacToe:
                 current_cell = board_list[board_size-j-1][j+i]
                 x_count, o_count = self.evaluate_cell(current_cell, x_count, o_count)
 
-                if self.evaluate_winner(x_count, o_count):
-                    print("diagonal / down")
-                    return self.evaluate_winner(x_count, o_count)
+                winner = self.evaluate_winner(x_count, o_count, points_to_win)
+                if winner:
+                    return winner
                 
 
     def switch_player(self):
@@ -214,4 +249,4 @@ class TicTacToe:
 
 
 if __name__ == "__main__":
-    TicTacToe().run()
+    TicTacToe().setup()
--- src/tkinteros/applications/tictactoe/tictactoe_gui.py
+++ src/tkinteros/applications/tictactoe/tictactoe_gui.py
@@ -1,15 +1,74 @@
 import customtkinter as ctk
 
+from tkinteros.theme import THEME_COLORS
+from tkinteros.theme import THEME_FONTS
+
 
 class TicTacToeGUI:
-    def __init__(self):
+    def __init__(self, start_callback, replay_callback):
         self.create_variables()
         self.create_window()
+        self.start_callback = start_callback
+        self.replay_callback = replay_callback
+        self.create_main_menu()
         
         
     def run(self):
         print("running")
         self.window.mainloop()
+
+    
+    def create_main_menu(self):
+        self.main_menu_frame = ctk.CTkFrame(self.window, width=self.window_width, 
+                                            height=self.window_height, fg_color=THEME_COLORS.primary)
+        self.main_menu_frame.pack()
+        header = ctk.CTkLabel(self.main_menu_frame, text="Tic Tac Toe", text_color=THEME_COLORS.font_color, 
+                              font=(THEME_COLORS.font_color, THEME_FONTS.large))
+        header.place(anchor="n", relx=0.5, rely=0.05)
+        
+        rows_label = ctk.CTkLabel(self.main_menu_frame, text="Grid Size", text_color=THEME_COLORS.font_color,
+                                  font=(THEME_FONTS.family, THEME_FONTS.big))
+        rows_label.place(anchor="center", relx=0.5, rely=0.52)
+        row_input = ctk.CTkSegmentedButton(self.main_menu_frame, text_color=THEME_COLORS.button_font_color, 
+                                            font=(THEME_FONTS.family, THEME_FONTS.big), 
+                                            selected_hover_color=THEME_COLORS.button_hover, 
+                                            fg_color=THEME_COLORS.button, unselected_color=THEME_COLORS.button, 
+                                            selected_color=THEME_COLORS.button_hover, values=["3x3", "5x5"], 
+                                            unselected_hover_color=THEME_COLORS.button)
+        row_input.place(anchor="center", relx=0.5, rely=0.62)
+
+        play_button = ctk.CTkButton(self.main_menu_frame, text="Play", text_color=THEME_COLORS.button_font_color, 
+                                    fg_color=THEME_COLORS.button, command=lambda: self.start_callback(row_input.get()),
+                                    font=(THEME_COLORS.font_color, THEME_FONTS.button_font_size), hover_color=THEME_COLORS.button_hover)
+        play_button.place(anchor="center", relx=0.5, rely=0.8)
+
+
+    def create_game_over_menu(self, header: str):
+        self.game_over_frame = ctk.CTkFrame(self.window, width=400, height=400, fg_color=THEME_COLORS.primary)
+        self.game_over_frame.pack()
+        game_over_text = ctk.CTkLabel(self.game_over_frame, text=header, text_color=THEME_COLORS.font_color,
+                                      font=(THEME_FONTS.family_bold, THEME_FONTS.large))
+        game_over_text.place(anchor="n", relx=0.5, rely=0.05)
+        play_again_button = ctk.CTkButton(self.game_over_frame, text="Play Again", fg_color=THEME_COLORS.button, 
+                                          text_color=THEME_COLORS.button_font_color, hover_color=THEME_COLORS.button_hover,
+                                          font=(THEME_FONTS.family_bold, THEME_FONTS.button_font_size), command=self.replay_callback)
+        play_again_button.place(anchor="center", relx=0.5, rely=0.7)
+
+
+    def show_main_menu(self):
+        self.main_menu_frame.pack()
+
+
+    def show_game_over_menu(self):
+        self.game_over_frame.pack()
+
+
+    def hide_main_menu(self):
+        self.main_menu_frame.pack_forget()
+
+
+    def hide_game_over_menu(self):
+        self.game_over_frame.pack_forget()
 
 
     def check_cell(self, cell_button: ctk.CTkButton, player: str):
