deaf2a9e2e13dcf42c810bd71a0a425220a3d81f
Simon Milata <milata.simon@gmail.com>
2025-08-02 19:16:53 +0200

    Add basic dynamic icon coloring - Basic icon coloring done - Currently only works for get_image method - Will update the other methods + make all files compatible

--- src/tkinteros/gui/file_widget.py
+++ src/tkinteros/gui/file_widget.py
@@ -1,11 +1,18 @@
-import customtkinter as ctk
+from typing import Callable
+
 from PIL import Image
+import customtkinter as ctk
+
+from tkinteros.file_management.file import File
+from tkinteros.theme import THEME_COLORS
 
 
 class TextFileWidget:
-    def __init__(self, File: object, desktop_frame, on_click_callback, icon):
-        self.icon = icon
-        self.file = File
+    def __init__(
+            self, file: File, desktop_frame: ctk.CTkFrame, on_click_callback: Callable, 
+            light_icon: Image, dark_icon: Image):
+        self.icon = ctk.CTkImage(light_image=light_icon, dark_image=dark_icon, size=(50, 50))
+        self.file = file
         self.on_click_callback = on_click_callback
 
         self.create_file_icon(desktop_frame)
@@ -13,13 +20,15 @@ class TextFileWidget:
 
     def create_file_icon(self, desktop_frame):
         """Creates the body/icon of the file"""
-        file_body_frame = ctk.CTkFrame(desktop_frame, width=75, height=100)
+        file_body_frame = ctk.CTkFrame(desktop_frame, width=75, height=100, fg_color="transparent")
         file_body_frame.place(x=self.file.x_pos, y=self.file.y_pos)
+
         file_body_button = ctk.CTkButton(
             file_body_frame, width=75, height=100, command=self.on_click, text="",
-            image=ctk.CTkImage(self.icon, size=(50, 50)), fg_color="transparent"
-        ) # TODO: replace placeholder image
+            image=self.icon, fg_color="transparent", bg_color="transparent", hover=False
+        )
         file_body_button.place(anchor="center", relx=0.5, rely=0.5)
+
         file_body_name_label = ctk.CTkLabel(file_body_frame, width=75, height=20, text=self.file.name)
         file_body_name_label.place(anchor="center", relx=0.5, rely=0.9)
 
