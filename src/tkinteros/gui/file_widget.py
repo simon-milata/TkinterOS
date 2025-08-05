085e2f3f9bd9db7dbe10c79f9d2aa5db859911e7
Simon Milata <milata.simon@gmail.com>
2025-08-05 21:25:41 +0200

    Change file widget to open on double click instead of using a CTkButton

--- src/tkinteros/gui/file_widget.py
+++ src/tkinteros/gui/file_widget.py
@@ -11,36 +11,46 @@ class TextFileWidget:
     def __init__(
             self, file: File, desktop_frame: ctk.CTkFrame, on_click_callback: Callable, 
             light_icon: Image, dark_icon: Image):
-        self.icon = ctk.CTkImage(light_image=light_icon, dark_image=dark_icon, size=(50, 50))
         self.file = file
         self.on_click_callback = on_click_callback
+        self.light_icon = light_icon
+        self.dark_icon = dark_icon
 
-        self.create_file_icon(desktop_frame)
-        self.create_binds()
+        self.create_file_widget(desktop_frame)
+        self.create_hover_binds()
+        self.create_callback_binds()
 
 
-    def create_file_icon(self, desktop_frame):
+    def create_file_widget(self, desktop_frame):
         """Creates the body/icon of the file"""
         self.file_body_frame = ctk.CTkFrame(desktop_frame, width=75, height=100, fg_color="transparent")
         self.file_body_frame.place(x=self.file.x_pos, y=self.file.y_pos)
 
-        self.file_body_button = ctk.CTkButton(
-            self.file_body_frame, width=75, height=100, command=self.on_click, text="",
-            image=self.icon, fg_color="transparent", bg_color="transparent", hover=False
+        image = ctk.CTkImage(light_image=self.light_icon, dark_image=self.dark_icon, size=(50, 50))
+
+        self.image_label = ctk.CTkLabel(
+            master=self.file_body_frame, image=image, text="",
+            fg_color="transparent", bg_color="transparent"
         )
-        self.file_body_button.place(anchor="center", relx=0.5, rely=0.5)
+        self.image_label.place(anchor="center", relx=0.5, rely=0.5)
 
         self.file_body_name_label = ctk.CTkLabel(self.file_body_frame, width=75, height=20, text=self.file.name)
         self.file_body_name_label.place(anchor="center", relx=0.5, rely=0.9)
 
 
-    def create_binds(self):
+    def create_hover_binds(self):
         self.file_body_frame.bind("<Enter>", self.hover_enter)
         self.file_body_frame.bind("<Leave>", self.hover_exit)
-        self.file_body_button.bind("<Enter>", self.hover_enter)
-        self.file_body_button.bind("<Leave>", self.hover_exit)
+        self.image_label.bind("<Enter>", self.hover_enter)
+        self.image_label.bind("<Leave>", self.hover_exit)
         self.file_body_name_label.bind("<Enter>", self.hover_enter)
         self.file_body_name_label.bind("<Leave>", self.hover_exit)
+
+
+    def create_callback_binds(self):
+        self.file_body_frame.bind("<Double-1>", self.on_click)
+        self.image_label.bind("<Double-1>", self.on_click)
+        self.file_body_name_label.bind("<Double-1>", self.on_click)
 
 
     def hover_enter(self, event=None):
@@ -51,5 +61,5 @@ class TextFileWidget:
         self.file_body_frame.configure(fg_color="transparent")
 
 
-    def on_click(self):
+    def on_click(self, event=None):
         self.on_click_callback(self.file.name)
