d58c71d0df61fa1b064b2df8dfa6b31509fdf28f
Simon Milata <milata.simon@gmail.com>
2025-08-05 21:08:14 +0200

    Add highlight effect on hover for file widgets on desktop

--- src/tkinteros/gui/file_widget.py
+++ src/tkinteros/gui/file_widget.py
@@ -16,21 +16,39 @@ class TextFileWidget:
         self.on_click_callback = on_click_callback
 
         self.create_file_icon(desktop_frame)
+        self.create_binds()
 
 
     def create_file_icon(self, desktop_frame):
         """Creates the body/icon of the file"""
-        file_body_frame = ctk.CTkFrame(desktop_frame, width=75, height=100, fg_color="transparent")
-        file_body_frame.place(x=self.file.x_pos, y=self.file.y_pos)
+        self.file_body_frame = ctk.CTkFrame(desktop_frame, width=75, height=100, fg_color="transparent")
+        self.file_body_frame.place(x=self.file.x_pos, y=self.file.y_pos)
 
-        file_body_button = ctk.CTkButton(
-            file_body_frame, width=75, height=100, command=self.on_click, text="",
+        self.file_body_button = ctk.CTkButton(
+            self.file_body_frame, width=75, height=100, command=self.on_click, text="",
             image=self.icon, fg_color="transparent", bg_color="transparent", hover=False
         )
-        file_body_button.place(anchor="center", relx=0.5, rely=0.5)
+        self.file_body_button.place(anchor="center", relx=0.5, rely=0.5)
 
-        file_body_name_label = ctk.CTkLabel(file_body_frame, width=75, height=20, text=self.file.name)
-        file_body_name_label.place(anchor="center", relx=0.5, rely=0.9)
+        self.file_body_name_label = ctk.CTkLabel(self.file_body_frame, width=75, height=20, text=self.file.name)
+        self.file_body_name_label.place(anchor="center", relx=0.5, rely=0.9)
+
+
+    def create_binds(self):
+        self.file_body_frame.bind("<Enter>", self.hover_enter)
+        self.file_body_frame.bind("<Leave>", self.hover_exit)
+        self.file_body_button.bind("<Enter>", self.hover_enter)
+        self.file_body_button.bind("<Leave>", self.hover_exit)
+        self.file_body_name_label.bind("<Enter>", self.hover_enter)
+        self.file_body_name_label.bind("<Leave>", self.hover_exit)
+
+
+    def hover_enter(self, event=None):
+        self.file_body_frame.configure(fg_color=THEME_COLORS.highlight)
+
+
+    def hover_exit(self, event=None):
+        self.file_body_frame.configure(fg_color="transparent")
 
 
     def on_click(self):
