19189ae7944448bd6a58f962309d0ef3b6173f51
Simon Milata <milata.simon@gmail.com>
2026-06-19 20:20:51 +0200

    Optimize rook movement and generalize movement range function
    - Optimized repetitive rook movement
    - Modified movement range function to be more general for future pieces

--- src/tkinteros/applications/chess/game_logic.py
+++ src/tkinteros/applications/chess/game_logic.py
@@ -2,7 +2,7 @@ def get_possible_moves(board: list[list[str]], piece_coords: tuple[int, int]) ->
     piece_row, piece_col = piece_coords
     piece = board[piece_row][piece_col]
     piece_color = "B" if piece.startswith("B") else "W"
-    opp_piece_color = "B" if piece.startswith("W") else "W"
+    opp_piece_col = "B" if piece.startswith("W") else "W"
     piece_type = piece[1]
 
     if not piece:
@@ -14,55 +14,7 @@ def get_possible_moves(board: list[list[str]], piece_coords: tuple[int, int]) ->
         possible_moves = [(piece_row + 1, piece_col) if piece_color == "B" else (piece_row - 1, piece_col)]
 
     if piece_type == "R":
-        # DOWN
-        i = 0
-        while True:
-            i += 1
-            if piece_row - i < 0:
-                break
-            if board[piece_row - i][piece_col][0] == opp_piece_color:
-                possible_moves.append((piece_row - i, piece_col))
-                break
-            if board[piece_row - i][piece_col] != "  " or board[piece_row - i][piece_col][0] == piece_color:
-                break
-            
-            possible_moves.append((piece_row - i, piece_col))
-        # UP
-        i = 0
-        while True:
-            i += 1
-            if piece_row + i >= len(board):
-                break
-            if board[piece_row + i][piece_col].startswith(opp_piece_color):
-                possible_moves.append((piece_row + i, piece_col))
-                break
-            if board[piece_row + 1][piece_col] != "  " or board[piece_row + i][piece_col][0] == piece_color:
-                break
-            possible_moves.append((piece_row + i, piece_col))
-        # RIGHT
-        i = 0
-        while True:
-            i += 1
-            if piece_col + i >= len(board):
-                break
-            if board[piece_row][piece_col + i][0] == opp_piece_color:
-                possible_moves.append((piece_row, piece_col + 1))
-                break
-            if board[piece_row][piece_col + i] != "  " or board[piece_row][piece_col + i][0] == piece_color:
-                break
-            possible_moves.append((piece_row, piece_col + i))
-        # LEFT
-        i = 0
-        while True:
-            i += 1
-            if piece_col - i < 0:
-                break
-            if board[piece_row][piece_col - i][0] == opp_piece_color:
-                possible_moves.append((piece_row, piece_col - 1))
-                break
-            if board[piece_row][piece_col - i] != "  " or board[piece_row][piece_col - i][0] == piece_color:
-                break
-            possible_moves.append((piece_row, piece_col - i))
+        possible_moves = get_move_range(piece_coords, opp_piece_col, board)
     
         
     while piece_coords in possible_moves:
@@ -76,3 +28,34 @@ def move_piece(board: list[list[str]], init_pos: tuple[int, int], dest_pos: tupl
     board[init_pos[0]][init_pos[1]] = "  "
     board[dest_pos[0]][dest_pos[1]] = piece
     return board
+
+
+def get_move_range(piece_coords: tuple[int, int], opp_piece_col: str, board: list[list[str]]):
+    piece_row, piece_col = piece_coords
+    possible_moves = []
+
+    directions = [
+        (-1, 0), # down
+        (1, 0), # up
+        (0, 1), # right
+        (0, -1), # left
+    ]
+
+    board_size = len(board)
+
+    for y, x in directions:
+        row, col = piece_row + y, piece_col + x
+
+        while 0 <= row < board_size and 0 <= col < board_size:
+            square = board[row][col]
+
+            if square == "  ":
+                possible_moves.append((row, col))
+            else:
+                if square[0] == opp_piece_col:
+                    possible_moves.append((row, col))
+                break
+
+            row += y
+            col += x
+    return possible_moves
