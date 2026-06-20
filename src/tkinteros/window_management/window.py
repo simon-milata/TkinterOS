02492c705213b00231d36c6c44cf6800aa50da1c
Simon Milata <milata.simon@gmail.com>
2026-06-20 23:36:10 +0200

    Improve custom window visuals
    - Changed window GUI to use theme colors
    - Added a window border
    - Changed overall look slightly

--- src/tkinteros/window_management/window.py
+++ src/tkinteros/window_management/window.py
@@ -1,6 +1,6 @@
 import customtkinter as ctk
 
-from src.tkinteros.theme import ThemeColors, ThemeFonts
+from src.tkinteros.theme import THEME_COLORS, THEME_FONTS
 
 
 class Window:
@@ -12,29 +12,36 @@ class Window:
 
 
     def create(self):
-        self.main_frame = ctk.CTkFrame(self.master, fg_color="red", width=500, height=520)
+        self.main_frame = ctk.CTkFrame(self.master, fg_color=THEME_COLORS.bright, width=500, height=520)
         self.main_frame.place(x=100, y=100)
+        self.inside_frame = ctk.CTkFrame(self.main_frame)
+        self.inside_frame.pack(padx=2, pady=2, fill="both", expand=True)
         self.create_top_bar()
+        self.content_frame = ctk.CTkFrame(self.inside_frame, fg_color="blue")
+        self.content_frame.pack(fill="both", expand=True)
     
 
     def create_top_bar(self):
-        top_bar = ctk.CTkFrame(self.main_frame, height=40, corner_radius=0, width=300)
+        top_bar = ctk.CTkFrame(
+            self.inside_frame, height=40, corner_radius=0, width=300, fg_color=THEME_COLORS.bright,
+            bg_color=THEME_COLORS.bright
+        )
         top_bar.pack(fill="x")
         top_bar.pack_propagate(False)
-        button_zone = ctk.CTkFrame(top_bar, width=120, height=40, corner_radius=0, fg_color="green")
+        button_zone = ctk.CTkFrame(top_bar, width=120, height=40, corner_radius=0, fg_color=THEME_COLORS.bright)
         button_zone.pack(side="right")
         button_zone.grid_propagate(False)
         close_button = ctk.CTkButton(
-            button_zone, width=40, height=40, text="X", hover_color="red", fg_color="orange", 
-            command=self.close_window
+            button_zone, width=40, height=32, text="X", hover_color=THEME_COLORS.highlight, 
+            fg_color=THEME_COLORS.off, corner_radius=5, font=(THEME_FONTS.family_bold, THEME_FONTS.small),
+            text_color=THEME_COLORS.font_color, bg_color=THEME_COLORS.bright, command=self.close_window
         )
-        close_button.grid()
+        close_button.pack(side="left", padx=10)
 
         top_bar.bind("<Button-1>", self.start_move)
         top_bar.bind("<B1-Motion>", self.move_window)
-
-        self.content_frame = ctk.CTkFrame(self.main_frame, fg_color="blue")
-        self.content_frame.pack(fill="both", expand=True)
+        button_zone.bind("<Button-1>", self.start_move)
+        button_zone.bind("<B1-Motion>", self.move_window)
 
 
     def start_move(self, event):
